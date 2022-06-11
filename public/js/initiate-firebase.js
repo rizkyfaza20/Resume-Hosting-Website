2790a1d3527f9068b820e52e6be9df0eba3c398c
faza <faza@n4tglobalinc.com>
2022-06-11 15:01:55 +0700

    added changes for run the firebase

--- public/js/initiate-firebase.js
+++ public/js/initiate-firebase.js
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getPerformance } from "firebase/performance";
+import * as firebase from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const perf = getPerformance(app);
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.perf();
