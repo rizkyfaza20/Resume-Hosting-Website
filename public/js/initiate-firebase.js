96a44f3ce61aae1015a340fe1dec6b07c8b68d77
faza <faza@n4tglobalinc.com>
2022-06-11 19:36:24 +0700

    added packages for importing firebase

--- public/js/initiate-firebase.js
+++ public/js/initiate-firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from 'firebase/app';
+import { initializeApp } from "@firebase/app";
+import { getPerformance } from "@firebase/performance";
+import { getAnalytics } from "@firebase/analytics"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,7 +9,7 @@ import * as firebase from 'firebase/app';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBtG-LZiM3FyteFokRwQmEoLfnmOOWLax8",
-  authDomain: "netdev-mrfzy.com",
+  authDomain: "netdev-mrfzy.cloud",
   projectId: "website-portfolio-devops",
   storageBucket: "website-portfolio-devops.appspot.com",
   messagingSenderId: "854791135110",
@@ -16,6 +18,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-firebase.perf();
+const app = initializeApp(app);
+const performance = getPerformance(app);
+const analytics = getAnalytics(app);
